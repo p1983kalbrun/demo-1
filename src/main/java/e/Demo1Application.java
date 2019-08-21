28310e493da94f74604f31647632ba54ebe0cb8a
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-08-21 14:39:56 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/e/Demo1Application.java
@@ -0,0 +1,12 @@
+package e;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
